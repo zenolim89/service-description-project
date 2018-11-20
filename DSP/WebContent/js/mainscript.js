e04d821a151181369693988be3b9d51ca2d5c123
m <m@192.168.0.18>
2018-11-20 09:08:41 +0900

    - change web sources

--- DSP/WebContent/js/mainscript.js
+++ DSP/WebContent/js/mainscript.js
@@ -2,6 +2,7 @@ var authRequest;
 var authJsonInfo;
 var svcRegRequest;
 var svcJsonInfo;
+var svcListInfo;
 
 // Ajax 인증 요청 초기화
 function createXMLHttpAuthReq() {
@@ -16,7 +17,7 @@ function createXMLHttpAuthReq() {
 function authReqFunction() {
 	createXMLHttpAuthReq();
 	AuthReqParam();
-	authRequest.open('POST', 'http://localhost:8080/DSP/auth');
+	authRequest.open('POST', './auth');
 	authRequest.setRequestHeader('Content-Type', 'application/json'); // 컨텐츠타입
 	authRequest.send(authJsonInfo);
 	authRequest.onreadystatechange = AuthRespProcess;
@@ -34,35 +35,45 @@ function AuthReqParam() {
 // Ajax 인증 응답 처리(Json)
 function AuthRespProcess() {
 	if (authRequest.readyState == 4 && authRequest.status == 200) {
-		alert(authRequest.responseText);
+		alert("[로그인 성공] \n" + "Detail : " + authRequest.responseText);
 		console.log(authRequest.responseText);
-		/*
-		 * document.getElementById("loginForm").style.display = "none"; // 로그인
-		 * document.getElementById("loginCompl").style.display = "block"; // 로그인
-		 * RegSVCList();
-		 */
+		LoginDisplay();
+		RegSVCList(authRequest.responseText);
 	}
 }
 
+// 로그인 성공 시 화면 UI 변경
+function LoginDisplay() {
+	var loginForm = document.getElementById("loginForm");
+	var loginCompl = document.getElementById("loginCompl");
+
+	if (loginForm.style.display == 'none') {
+		loginForm.style.display = 'block';
+		loginCompl.style.display = 'none';
+	} else {
+		loginForm.style.display = 'none';
+		loginCompl.style.display = 'block';
+	}
+
+	var ul = document.getElementById('svcListInfo');
+	if (ul)
+		while (ul.firstChild) {
+			ul.removeChild(ul.firstChild);
+		}
+}
+
 // 계정에 등록된 서비스 목록
-function RegSVCList() {
-	var ul = document.getElementById("svcList");
-	ul.innerHTML = "";
-	var li1 = document.createElement("li");
-	li1.appendChild(document.createTextNode("- (서비스 명)"));
-	ul.appendChild(li1);
+function RegSVCList(loginResult) {
+	var jsonObj = JSON.parse(loginResult);
+	var ul = document.getElementById("svcListInfo");
 
-	var li2 = document.createElement("li");
-	li2.appendChild(document.createTextNode("- (서비스 명)"));
-	ul.appendChild(li2);
-
-	var li3 = document.createElement("li");
-	li3.appendChild(document.createTextNode("- (서비스 명)"));
-	ul.appendChild(li3);
-
-	var li4 = document.createElement("li");
-	li4.appendChild(document.createTextNode("- (서비스 명)"));
-	ul.appendChild(li4);
+	for (var i = 0; i < jsonObj.regiServiceInfo.length; i++) {
+		var svcCodeName = jsonObj.regiServiceInfo[i].serviceCode;
+		var li = document.createElement("li");
+		li.innerHTML = "<li class='nav-item'><a href='' class='nav-link'>"
+				+ svcCodeName + "</a></li>";
+		ul.appendChild(li);
+	}
 }
 
 /*
@@ -85,32 +96,35 @@ function RegSVCList() {
  * "svcevtPrprtyVal": "1" } ] } ] } ] }
  */
 
-//데이터포맷 테이블 행 추가
+// 데이터포맷 테이블 행 추가
 function addRow(id) {
 	var tbody = document.getElementById(id);
 	var row = document.createElement("tr");
 	row.setAttribute("name", "dataFormatArr");
 
 	var td1 = document.createElement("td");
-	td1.innerHTML = "<input type=text placeholder='keyName' name='keyName' size=12 align=absmiddle >";
+	td1.innerHTML = "<input type=text class='form-control' placeholder='keyName' name='keyName' size=12 align=absmiddle >";
 
 	var td2 = document.createElement("td");
-	td2.innerHTML = "<input type=text placeholder='valueName' name='valueName' size=12 align=absmiddle >";
+	td2.innerHTML = "<input type=text class='form-control' placeholder='valueName' name='valueName' size=12 align=absmiddle >";
 
 	var td3 = document.createElement("td");
-	td3.innerHTML = "<input type=text placeholder='superVar' name='superVar' size=12 align=absmiddle >";
+	td3.innerHTML = "<input type=text class='form-control'placeholder='superVar' name='superVar' size=12 align=absmiddle >";
 
 	var td4 = document.createElement("td");
-	td4.innerHTML = "<select name='type'> <option value='' disabled>데이터 타입 선택</option> <option value='JSONObject'>JSONObject</option> <option value='JSONArray'>JSONArray</option> </select>";
+	td4.innerHTML = "<select name='type' class='form-control'> <option value='' disabled>데이터 타입 선택</option> <option value='JSONObject'>JSONObject</option> <option value='JSONArray'>JSONArray</option> </select>";
 
 	var td5 = document.createElement("td");
-	td5.innerHTML = "<input type=text placeholder='userDefine' name='userDefine' size=12 align=absmiddle >";
+	td5.innerHTML = "<input type=text class='form-control'placeholder='userDefine' name='userDefine' size=12 align=absmiddle >";
 
 	var td6 = document.createElement("td");
-	td6.innerHTML = "<select name='subArrType'> <option value='' disabled>데이터 타입 선택</option> <option value='JSONObject'>JSONObject</option> <option value='JSONArray'>JSONArray</option> </select>";
+	td6.innerHTML = "<select name='subArrType'class='form-control'> <option value='' disabled>sub 데이터 타입 선택</option> <option value='object'>Object</option> <option value='string'>string</option> <option value='false'>none</option> </select>";
 
 	var td7 = document.createElement("td");
-	td7.innerHTML = "<input type=text placeholder='subArr' name='subArr' size=12 align=absmiddle >";
+	td7.innerHTML = "<input type=text class='form-control' placeholder='subArr' name='subArr' size=12 align=absmiddle >";
+
+	var td8 = document.createElement("td");
+	td8.innerHTML = "<input class='btn btn-danger' type='button' onclick='deleteLine(this);' value='삭제'>";
 
 	row.appendChild(td1);
 	row.appendChild(td2);
@@ -119,12 +133,21 @@ function addRow(id) {
 	row.appendChild(td5);
 	row.appendChild(td6);
 	row.appendChild(td7);
+	row.appendChild(td8);
 	tbody.appendChild(row);
 }
 
-//서비스 등록 정보 Json으로 변환
+function deleteLine(obj) {
+	var tr = $(obj).parent().parent();
+	// 라인 삭제
+	tr.remove();
+}
+
+// 서비스 등록 정보 Json으로 변환
 function SVCReqParam() {
 	var svcInfo = new Object();
+	var NEInfo = new Object();
+
 	svcInfo.userAuth = document.getElementsByName('userAuth')[0].value;
 
 	var interfaceType = document.getElementsByName("interfaceType");
@@ -154,7 +177,7 @@ function SVCReqParam() {
 			dialogArray1.push(facilitiesItems[i].value);
 	}
 	if (dialogArray1.length)
-		svcInfo.facilitiesItems = dialogArray1;
+		NEInfo.facilitiesItems = dialogArray1;
 
 	var liveWebcamsItems = document.getElementsByName("liveWebcamsItems");
 	for (var i = 0; i < liveWebcamsItems.length; i++) {
@@ -162,7 +185,7 @@ function SVCReqParam() {
 			dialogArray2.push(liveWebcamsItems[i].value);
 	}
 	if (dialogArray2.length)
-		svcInfo.liveWebcamsItems = dialogArray2;
+		NEInfo.liveWebcamsItems = dialogArray2;
 
 	var resortItems = document.getElementsByName("resortItems");
 	for (var i = 0; i < resortItems.length; i++) {
@@ -170,7 +193,8 @@ function SVCReqParam() {
 			dialogArray3.push(resortItems[i].value);
 	}
 	if (dialogArray3.length)
-		svcInfo.resortItems = dialogArray3;
+		NEInfo.resortItems = dialogArray3;
+	svcInfo.refDialog = NEInfo;
 
 	var method = document.getElementsByName("method")[0];
 	svcInfo.method = method.options[method.selectedIndex].value;
@@ -201,7 +225,7 @@ function SVCReqParam() {
 	console.log(svcJsonInfo);
 }
 
-//Ajax 서비스등록 요청 초기화
+// Ajax 서비스등록 요청 초기화
 function createXMLHttpSVCReq() {
 	if (window.ActiveXObject) {
 		svcRegRequest = new ActiveXObject("Microsoft.XMLHTTP");
@@ -214,13 +238,13 @@ function createXMLHttpSVCReq() {
 function svcReqFunction() {
 	createXMLHttpSVCReq();
 	SVCReqParam();
-	svcRegRequest.open('POST', 'http://localhost:8080/DSP/registration');
+	svcRegRequest.open('POST', './registration');
 	svcRegRequest.setRequestHeader('Content-Type', 'application/json'); // 컨텐츠타입
 	svcRegRequest.send(svcJsonInfo);
 	svcRegRequest.onreadystatechange = svcRespProcess;
 }
 
-//Ajax 서비스등록 응답 처리(Json)
+// Ajax 서비스등록 응답 처리(Json)
 function svcRespProcess() {
 	if (svcRegRequest.readyState == 4 && svcRegRequest.status == 200) {
 		alert(svcRegRequest.responseText);
@@ -232,5 +256,3 @@ function svcRespProcess() {
 		 */
 	}
 }
-
-

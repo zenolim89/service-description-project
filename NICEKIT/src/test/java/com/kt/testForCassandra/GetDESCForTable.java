d02768840f9db5cf18c7d26a28338569171f1bac
Minwoo Ryu <minwoo.kt@gmail.com>
2019-02-11 17:41:47 +0900

    - developing test code for get description of column familiy

---
+++ NICEKIT/src/test/java/com/kt/testForCassandra/GetDESCForTable.java
@@ -0,0 +1,64 @@
+package com.kt.testForCassandra;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.cassandra.thrift.Column;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ColumnMetadata;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.TableMetadata;
+import com.kt.dataDao.ConnectToCanssandra;
+
+public class GetDESCForTable {
+	
+	
+	public static void main (String arg[]) {
+	
+		ConnectToCanssandra connDB = new ConnectToCanssandra();
+		
+		Cluster cluster = connDB.getCluster();
+		Session session = cluster.connect();
+		
+		ResultSet res = null;
+		
+		TableMetadata tb = cluster.getMetadata()
+				.getKeyspace("commonks")
+				.getTable("intentinfo");
+		
+		
+			
+		List<ColumnMetadata> dataList = tb.getColumns();
+		List<ColumnMetadata> keyList = tb.getPartitionKey();
+		
+		
+		for (ColumnMetadata key : keyList) {
+			
+			System.out.println("KEYS: " + key.getName());
+		}
+				
+		
+		for (ColumnMetadata data : dataList) {
+			
+			System.out.println("TYPE: " + data.getType());
+			System.out.println("NAME: " + data.getName());
+				
+			
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+			
+	
+	
+
+}

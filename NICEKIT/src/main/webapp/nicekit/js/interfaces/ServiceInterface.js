63b4ad510f005328350976aa8248d2de1cabb908
hosung <zenolim89@gmail.com>
2019-02-14 18:22:31 +0900

    update project

--- NICEKIT/src/main/webapp/nicekit/js/interfaces/ServiceInterface.js
+++ NICEKIT/src/main/webapp/nicekit/js/interfaces/ServiceInterface.js
@@ -257,7 +257,7 @@ gigagenie.voice.onRequestClose = function() {
 function SpeechINTRC(appId) {
 	gigagenie.voice.onActionEvent = function(extra) {
 		var sentence = extra.uword;
-		alert("인식문장 : " + sentence);
+		alert("인식문장 : " + sentence); //발화 문장
 		switch (extra.actioncode) {
 			case 'HotelAmenityItem':
 				svcReqFunction('rsrt001', extra.actioncode, extra.parameter['NE-AMENITY']);
